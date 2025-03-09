fcad0c8a229728d90272ff09447728dbdb4b13e3
zhixiangjuan <157693619+zhixiangjuan@users.noreply.github.com>
2025-03-09 19:31:18 +0800

    Add files via upload

---
+++ assignment2/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] num = new int[100];
+        num[0] = 1;
+        for(int i=1;i<100;i++)
+        {
+            if (num[i]==0)
+                for(int j=i+1;j<100;j++)
+                    if ((j + 1) % (i + 1) == 0) num[j] = 1;
+        }
+        for (int i = 0; i < 100; i++)
+            if (num[i] == 0) Console.Write((i + 1) + " ");
+    }
+}
---
+++ assignment2/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("请输入矩阵的高");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine("请输入矩阵的宽");
+        int n = int.Parse(Console.ReadLine());
+        int[][] num = new int[m][];
+        for(int i=0;i<m;i++)
+        {
+            num[i] = new int[n];
+            string[] str = Console.ReadLine().Split(' ');
+            for (int j = 0; j < n; j++)
+                num[i][j] = int.Parse(str[j]);
+        }
+        if (m != n) { Console.WriteLine("false"); return; }
+        for (int i = 0; i < m-1; i++)
+            if (num[i][i] != num[i + 1][i+1]) { Console.WriteLine("false"); return; }
+        Console.WriteLine("true"); return;
+    }
+}
