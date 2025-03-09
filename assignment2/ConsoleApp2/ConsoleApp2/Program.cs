a94fe2b8ab1de42ef23acb94c7f3f816aba013c0
zhixiangjuan <157693619+zhixiangjuan@users.noreply.github.com>
2025-03-09 19:27:53 +0800

    Add files via upload

---
+++ assignment2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        bool IsPrime(int n)
+        {
+            if (n < 2)
+                return false;
+            for (int i = 2; i <= n / i; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+        void f(int n)
+        {
+            if (IsPrime(n))
+            {
+                Console.WriteLine(n);
+                return;
+            }
+            for (int i=2;i<=n/i;i++)
+            {
+                if(n%i==0&&IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                    f(n / i);
+                    return;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个正整数");
+            int num = int.Parse(Console.ReadLine());
+            if(num<2)
+            {
+                Console.WriteLine(num + "没有质数因子");
+                return;
+            }
+            var pro = new Program();
+            pro.f(num);
+        }
+    }
+}
---
+++ assignment2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 0;
+        do
+        {
+            Console.WriteLine("请输入数组长度");
+            n = int.Parse(Console.ReadLine());
+        } while (n < 1);
+        int[] arr = new int[n];
+        Console.WriteLine("请输入数组元素，以空格隔开");
+        string[] str = Console.ReadLine().Split(' ');
+        int max = Convert.ToInt32(str[0]);
+        int min = Convert.ToInt32(str[0]);
+        int sum = 0;
+        for (int i=0;i<n;i++)
+        {
+            arr[i] = Convert.ToInt32(str[i]);
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+            sum += arr[i];
+        }
+        int aver = sum / n;
+        Console.WriteLine("最大值：" + max + " 最小值：" + min + " 平均值：" + aver + " 总和：" + sum);
+    }
+}
